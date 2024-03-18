0ef641f18cb5ef1b2e244c8d314b58db524fceef
hieubq <hieubq>
2024-03-19 01:10:46 +0700

    update ComicInfo

--- src/app/comic/[comic_id]/ComicInfo.tsx
+++ src/app/comic/[comic_id]/ComicInfo.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
-import Link from 'next/link'
-import { z } from 'zod'
+
+import { Icon } from '~/components/commons/Icon'
 import { IComic } from '~/types'
 
+import Authors from './Authors'
 import Genres from './Genres'
+import Stats from './Stats'
 
 const ComicInfo = ({ comic }: { comic: IComic }) => {
   return (
-    <div className="absolute top-0 inset-x-0 h-80 bg-gradient-to-b from-emerald-100 -z-10">
+    <>
+      <div className="absolute top-0 inset-x-0 h-80 bg-gradient-to-b from-emerald-100 -z-10" />
       <div className="max-w-5xl mx-auto border-4 border-transparent p-0 rounded-xl sm:grid sm:grid-cols-4 gap-6 md:p-4 md:border-white">
         <div className="aspect-[2/3] w-56 mx-auto sm:w-full rounded-lg border-2 overflow-hidden border-emerald-500 relative sm:col-span-1">
           <img
@@ -23,10 +26,34 @@ const ComicInfo = ({ comic }: { comic: IComic }) => {
             {Array.isArray(comic.other_names) ? comic.other_names.join(' | ') : comic.other_names}
           </p>
           <Genres genres={comic.genres} />
+          <Authors authors={comic.authors} />
+          <Stats
+            total_views={comic.total_views}
+            followers={comic.followers}
+          />
+          {comic.description && (
+            <div className="mt-2">
+              <p className="line-clamp-4">{comic.description.replace(/NetTruyen/g, 'NextComics')}</p>
+            </div>
+          )}
+          <div className="flex flex-col sm:flex-row items-center gap-3 mt-5 font-bold">
+            <button
+              className={`flex items-center gap-1 border-2 rounded text-white text-lg px-6 py-2 ${
+                comic.chapters.length ? 'border-emerald-500 bg-emerald-500' : 'border-gray-500 bg-gray-500'
+              }`}
+              disabled={!comic.chapters.length}
+            >
+              <Icon
+                name="radix/history"
+                className="text-lg mr-2"
+              />
+              Đọc ngay
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
-export default Genres
+export default ComicInfo
--- src/app/comic/[comic_id]/page.tsx
+++ src/app/comic/[comic_id]/page.tsx
@@ -2,12 +2,21 @@
 
 import { comicsClient } from '~/app/providers'
 
+import ComicInfo from './ComicInfo'
+
 export default function Page({ params }: { params: { comic_id: string } }) {
   const { comic_id } = params
 
   const comidDetail = comicsClient.comics.detail.useQuery(['detail', comic_id], { params: { comic_id } })
 
-  return <div className="relative pt-12 px-4 min-h-screen">
-    <
-  </div>
+  return (
+    <div className="relative pt-12 px-4 min-h-screen">
+      {comidDetail.isLoading ? 'Loading' : null}
+      {!comidDetail.isLoading && comidDetail.data ? (
+        <>
+          <ComicInfo comic={comidDetail.data?.body} />
+        </>
+      ) : null}
+    </div>
+  )
 }
